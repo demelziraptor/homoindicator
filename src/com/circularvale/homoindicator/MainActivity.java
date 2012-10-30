eaf800e5d617d3852c01104d8768f46235ad7db7
Demelziraptor <demelza@circularvale.com>
2012-10-30 13:50:50 +0000

    start second activity

--- src/com/circularvale/homoindicator/MainActivity.java
+++ src/com/circularvale/homoindicator/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
+    public final static String EXTRA_MESSAGE = "com.circularvale.homoindicator.MESSAGE";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,10 @@ public class MainActivity extends Activity {
     
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+				EditText editText = (EditText) findViewById(R.id.edit_name);
+				String message = editText.getText().toString();
+				intent.putExtra(EXTRA_MESSAGE, message);
+				startActivity(intent);
     }
 }
